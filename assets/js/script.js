704084d187650cad1fb5b49672300f7e2a9ea06f
faeyza <faeyzaardellein@gmail.com>
2026-01-17 17:17:53 +0700

    countdown styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,81 @@
 
-const texts = ["3","2","1","Happy Birthday Adel","08.02.07","Happy 19+"];
+const scenes = [
+  { text: "3", duration: 800 },
+  { text: "2", duration: 800 },
+  { text: "1", duration: 800 },
+  { text:"Happy Birthday Adel", duration: 1500 },
+  { text:"08.02.07", duration: 1500, switchRain: true },
+  { text:"Happy 19+", duration: 2000 },
+];
 
 let index = 0;
 const textEl = document.getElementById("text");
 
-function showText() {
-  textEl.textContent = texts[index];
+/* canvas setup*/
+const  canvas = document.getElementById("rain");
+const ctx = canvas.getContext("2d");
+
+function resize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resize();
+window.addEventListener("resize", resize);
+
+/* rain setup*/
+let rainChars = ["0", "1"];
+const fontSize = 18;
+let columns = Math.floor(canvas.width / fontSize);
+let drops = Array(columns).fill(1);
+
+function drawRain() {
+  ctx.fillStyle = "rgba(0,0,0,0.09)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "rgba(225, 182, 193, 0.73)";
+  ctx.font = fontSize + "px monospace";
+
+  for (let i = 0; i < drops.length; i++) {
+    const char = rainChars[Math.floor(Math.random()* rainChars.length)];
+    ctx.fillText(char, i * fontSize, drops[i]* fontSize);
+
+    if (drops[i]* fontSize > canvas.height && Math.random() > 0.975) {
+      drops[i] = 0;
+    }
+    drops[i] += Math.random() * 1.0 + 0.3;
+  }
+}
+
+setInterval(drawRain, 33);
+
+/* scene controller */
+
+function showScene() {
+  const scene = scenes[index];
+
+  textEl.textContent = scene.text;
   textEl.style.opacity = 1;
+
+  if (scene.text === "3" || scene.text === "2" || scene.text === "1"){
+    textEl.style.fontSize = "170px";
+  }else {
+    textEl.style.fontSize = "90px";
+  }
+
+  //switch biner ke huruf
+  if (scene.switchRain) {
+    rainChars = ["H", "A","P","P","Y","B","I","R","T","H","D","A","Y",]
+  }
 
   setTimeout(() => {
     textEl.style.opacity = 0;
     index++;
 
-    if (index < texts.length) {
-     setTimeout(showText,600);//jeda antar text
+    if (index < scenes.length) {
+     setTimeout(showScene,600);//jeda antar text
     }
-  },1200);//durasi text tampil
+  }, scene.duration);
 }
 
-showText();
+showScene();
+
